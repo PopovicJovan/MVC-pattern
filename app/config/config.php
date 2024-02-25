5eda309071d8354bf92395bf0ed21dfe7b81f419
PopovicJovan <jovanp.school06@gmail.com>
2024-02-25 12:05:44 +0100

    MVC-pattern

--- app/config/config.php
+++ app/config/config.php
@@ -3,11 +3,11 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
-    define('DB_NAME', 'tmvc');
+    define('DB_NAME', 'proba');
 
     // App root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL root
-    define('URLROOT', 'http://localhost/MVC');
+    define('URLROOT', 'http://localhost/vjezbavjezba');
     // Site name
-    define('SITENAME', 'MyMVC');
+    define('SITENAME', 'VJEZBA');
--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
     require_once '../app/bootstrap.php';
-//C:\xampp\htdocs\MVC\app\bootstrap.php
+
     $init = new Core();
